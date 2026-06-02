f25ac4e826527f4b30d1b9d9110b271953d27402
jinliang <jinliang@f6r11s2.paris.42.school>
2026-06-02 16:48:54 +0200

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cctype>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j] != '\0'; j++)
+			{
+				// 使用<cctype>中的 std::toupper 将字符转为大写
+				std::cout << static_cast<char>(std::toupper(av[i][j]));
+			}
+		}
+	}
+	// 无论如何，最后都必须以换行符结尾
+	std::cout << std::endl;
+	return 0;
+}
+
